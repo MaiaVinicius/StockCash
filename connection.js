d59e96bf3f23f6c38dd0378b774907efc82edb93
Vinicius <viniciusmaia.tx@gmail.com>
2017-07-26 20:58:16 -0300

    mudei o title

--- connection.js
+++ connection.js
@@ -2,18 +2,22 @@
  * Created by MaiaVinicius on 25/07/17.
  */
 
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'm3bkfz3b',
-    database : 'simuladorbovespadb'
+    host: 'localhost',
+    user: 'root',
+    password: 'm3bkfz3b',
+    database: 'simuladorbovespadb'
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+/*try {
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
+}catch (err){
+    console.log(err)
+}*/
 
 
 module.exports = connection;
