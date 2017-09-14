5a0cbb36cce9ff08b92c03b7586049cbe8173b6d
renatobcj <zerenatobaroni@gmail.com>
2017-09-14 00:31:09 -0300

    se nao for eu n sei mais oq fazer

--- connection.js
+++ connection.js
@@ -1,15 +1,36 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'us-cdbr-iron-east-05.cleardb.net',
-    user: 'bf0081729740f5',
-    password: '85d2fef8',
-    database: 'heroku_9a0ab98794b1b1e'
+
+var pool = mysql.createPool({
+    host     : 'us-cdbr-iron-east-05.cleardb.net',
+    user     : 'bf0081729740f5',
+    password : '85d2fef8',
+    database : 'heroku_9a0ab98794b1b1e'
 });
 
-connection.connect(function (err) {
-    if (err) console.log(err);
-    console.log("Connected!");
-});
-
-
-module.exports = connection;
+module.exports = {
+    query: function(){
+        var sql_args = [];
+        var args = [];
+        for(var i=0; i<arguments.length; i++){
+            args.push(arguments[i]);
+        }
+        var callback = args[args.length-1]; //last arg is callback
+        pool.getConnection(function(err, connection) {
+            if(err) {
+                console.log(err);
+                return callback(err);
+            }
+            if(args.length > 2){
+                sql_args = args[1];
+            }
+            connection.query(args[0], sql_args, function(err, results) {
+                connection.release(); // always put connection back in pool after last query
+                if(err){
+                    console.log(err);
+                    return callback(err);
+                }
+                callback(null, results);
+            });
+        });
+    }
+};
